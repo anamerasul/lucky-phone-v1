e30d024b5de88d526d0a9b72365da6d28b035ed3
anamerasul <anamerasul@gmail.com>
2022-03-26 02:44:19 +0600

    product component create

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Shop.css'
 
 const Shop = () => {
+
+    useEffect(()=>{
+        fetch(`PhoneFakedata.json`)
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
+
+
     return (
         <div>
-            
+            <h1>SHOP</h1>
         </div>
     );
 };
