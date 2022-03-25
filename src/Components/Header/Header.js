40d4555676255097f97fed7b2a652003b5a3c94a
anamerasul <anamerasul@gmail.com>
2022-03-26 02:34:56 +0600

    shop js created

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                 <a className="navbar-brand" href="/">Lucky Phone</a>
+            </div>
+        </nav>
+            
+        </div>
+    );
+};
+
+export default Header;
