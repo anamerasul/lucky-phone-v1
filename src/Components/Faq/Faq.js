ddd4128dc9864b22d092aa1e99e5ed293a4c7f93
anamerasul <anamerasul@gmail.com>
2022-03-27 09:26:58 +0600

    How useState work? question answer  in faq

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -65,11 +65,15 @@ const Faq = () => {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="flush-headingThree">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            Accordion Item #3
+                            How useState work?
                         </button>
                     </h2>
                     <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        <div className="accordion-body">
+                            <p>
+                                useState may be a Hook that enables you to own state variables in practical elements. You pass the initial state this function perform and it returns a variable with the current state worth (not essentially the initial state) and another perform to update this value.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
